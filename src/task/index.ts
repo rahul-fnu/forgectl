81cf66b6e6669a80b29043b22e4ba564b5b867ac
forgectl <forge@localhost>
2026-03-22 19:20:13 +0000

    [forge] You are an autonomous coding agent working inside a Docker container. Yo

--- src/task/index.ts
+++ src/task/index.ts
@@ -13,3 +13,5 @@ export { TaskSpecSchema, ValidatedTaskSpec } from "./schema.js";
 export { loadTaskSpec, loadTaskSpecFromString, findTaskSpecs } from "./loader.js";
 export { validateTaskSpec } from "./validator.js";
 export { scaffoldTaskSpec } from "./scaffold.js";
+export type { DecompositionStrategy, DecompositionResult } from "./decomposition.js";
+export { ModuleBoundaryStrategy, decompose } from "./decomposition.js";
